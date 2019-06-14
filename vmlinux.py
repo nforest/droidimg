e4596ab2e31a7267e740a0c83fc5bb2216bc688b
idl3r <idler1984@gmail.com>
2019-06-14 20:32:21 +0800

    align start address with 64kb on arm64

--- vmlinux.py
+++ vmlinux.py
@@ -233,6 +233,8 @@ def do_guess_start_address(kallsyms, vmlinux):
     # print_log('[+]kallsyms_guess_start_addresses = ',  hex(0xffffff8008000000 + INT(8, vmlinux)) if kallsyms['arch']==64 else '', hex(_startaddr_from_banner), hex(_startaddr_from_processor), hex(_startaddr_from_xstext))
     
     for addr in start_addrs:
+        if kallsyms['arch'] == 64:
+            addr = addr & 0xffffffffffff0000
         if addr != 0 and addr % 0x1000 == 0:
             kallsyms['_start']= addr
             break
